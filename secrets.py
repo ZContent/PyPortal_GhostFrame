48cdbd39dc365b970ad738fa3f33caa7424841c4
Dan Cogliano <cogliano@users.noreply.github.com>
2019-04-04 14:56:19 -0400

    Create secrets.py
    initial commit

---
+++ secrets.py
@@ -0,0 +1,7 @@
+# Ghost Frame only needs ssid and password, but other programs that use this file
+# may have additional fields
+secrets = {
+    'ssid' : 'home ssid',
+    'password' : 'my password',
+    'timezone' : "America/New_York" # http://worldtimeapi.org/timezones
+    }
